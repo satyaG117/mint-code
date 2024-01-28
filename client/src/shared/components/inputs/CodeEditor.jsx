9e144b3008fe5a314c6228e808ab639d7543250c
satyaG117 <goswamisatya2003@gmail.com>
2024-01-28 14:32:35 +0530

    fixed syntax highlighting bug

--- client/src/shared/components/inputs/CodeEditor.jsx
+++ client/src/shared/components/inputs/CodeEditor.jsx
@@ -1,8 +1,9 @@
 import Editor from '@monaco-editor/react';
 
-export default function CodeEditor({language, value='', onChange, theme='vs-dark'}) {
+export default function CodeEditor({ language='c', value = '', onChange, theme = 'vs-dark' }) {
     return (
         <Editor
+            key={language}
             height="100%"
             defaultLanguage={language}
             value={value}
